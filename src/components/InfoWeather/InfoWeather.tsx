03fb12ebdeb78adf8891a214101ce65601480fa0
paolaozv <paolaozv@gmail.com>
2020-11-24 00:51:26 +0100

    remove unused files and add global styles

---
+++ src/components/InfoWeather/InfoWeather.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './InfoWeather.module.css';
+
+const InfoWeather = () => {
+  return (
+    <div className={styles.container}>
+
+    </div>
+  );
+};
+
+export default InfoWeather;
---
+++ src/components/SummaryWeather/SummaryWeather.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './SummaryWeather.module.css';
+
+const SummaryWeather = () => {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  );
+};
+
+export default SummaryWeather;
